c8a820c2b631434c7900a27ea7f17da909564df2
Krzysztof Langner <klangner@gmail.com>
2018-12-09 13:04:55 +0100

    Added comments to sample bot script

--- chapter_1/simple_chatbot.py
+++ chapter_1/simple_chatbot.py
@@ -1,17 +1,27 @@
 import re
 
 
+# List of possible chatbot names.
+# Can be used in phrases as 'Hello Rose'
 MY_NAMES = {'rosa', 'rose', 'chatty', 'chatbot', 'bot', 'chatterbot'}
 
+# It is also possible to use those names, but chatbot doesn't really like them
 CURT_NAMES = {'hal', 'you', 'u'}
 
+# Our chatbot is rule based, and the rules are based on regular expression.
+# The following pattern checks if the phrase use a greeting.
+# If it is, then we expect that the last group will contain name given to the chatbot
 GREETING_PATTERN = re.compile(
     r"[^a-z]*([y]o|hi|[h']?ello|ok|hey|(good[ ])?(morn[gin']{0,3}|afternoon|even[gin']{0,3}))[\s,;:]{1,3}([a-z]{1,20})",
     flags=re.IGNORECASE)
 
+# This pattern will check if the phrase ends conversation lie 'bye'
 LEAVE_PATTERN = re.compile(r"bye.*", flags=re.IGNORECASE)
 
 
+# Main loop:
+#   * We read the text from the console
+#   * Based on entered phrase give answer
 def main():
     greeter_name = None
     while True:
@@ -31,4 +41,5 @@ def main():
                 print("Rosa: Sorry I don't understand")
 
 
+# Start app
 main()
